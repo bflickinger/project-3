80c9436f81bd54ba0a3772433903cb463836ccaa
bflickinger <barry.flickinger@gmail.com>
2019-04-17 14:52:27 -0700

    derp

--- client/src/components/flash/FlashMessage.js
+++ client/src/components/flash/FlashMessage.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Modal, Button } from 'react-materialize';
 import PropTypes from "prop-types";
-import classnames from 'classnames';
 
 class FlashMessage extends Component {
   constructor(props) {
