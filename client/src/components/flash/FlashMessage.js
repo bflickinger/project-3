86601efa80108de21474c374377846d0e9a79ef0
bflickinger <barry.flickinger@gmail.com>
2019-04-16 21:27:49 -0700

    added modal

--- client/src/components/flash/FlashMessage.js
+++ client/src/components/flash/FlashMessage.js
@@ -13,26 +13,20 @@ class FlashMessage extends Component {
     this.props.deleteFlashMessage(this.props.message.id);
   }
 
-  isNewUser() {
+  componentDidMount() {
     if(typeof this.props.message.id != 'undefined'){
-      console.log("flashmessage isnewuser -> undefined");
+      console.log("flashmessage this.props.message.id ->", this.props.message.id);
+      this.modalButton.click();
     }
   }
 
   render() {
     const { id, type, text } = this.props.message;
-    const trigger = this.props.newUser;
-    console.log('flashmessage props ->', trigger);
-    // <Button onClick={this.onClick}>New Player!</Button>;
+    // const trigger =<Button onClick={this.onClick}>New Player!</Button>;
+    const trigger =<button onClick={this.onClick} className="hide" ref={button => this.modalButton = button}></button>
     return (
-      <div className={classnames('alert', {
-        'alert-success': type === 'success',
-        'alert-danger': type === 'error'
-      })}>
-        {console.log(this.props.message)}
-        <button onClick={this.onClick} className="close"><span>&times;</span></button>
-        {text}
-        <Modal header="Registration Successful!" trigger={trigger}>
+      <div>
+        <Modal id="newUser" header="Registration Successful!" trigger={trigger}>
           {text}
         </Modal>
       </div>
