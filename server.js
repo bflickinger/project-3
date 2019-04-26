6d822de1e8ab1659e8a3d75d0423024793dd839e
bflickinger <barry.flickinger@gmail.com>
2019-04-26 15:59:20 -0700

    heroku

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ const app = express();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 // Bodyparser middleware
