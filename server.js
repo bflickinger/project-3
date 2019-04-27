d44f69113d4952bd9d7d5ff2bfe33c8c2090d6b1
bflickinger <barry.flickinger@gmail.com>
2019-04-26 17:35:21 -0700

    ugh

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'), function(err) {
+    res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
