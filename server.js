223425d061c11cae514a49ad95beaaac57ddc7e0
bflickinger <barry.flickinger@gmail.com>
2019-04-26 17:42:05 -0700

    ugh

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const users = require("./routes/api/users");
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+    res.sendFile(path.join(__dirname, 'build', index.html), function(err) {
       if (err) {
         res.status(500).send(err)
       }
